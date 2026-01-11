99c1f0ac1c36ecd73da55938d664f295b09059c7
kurltk88-oss <kurltk88@gmail.com>
2026-01-11 22:47:47 +0000

    page names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
+st.set_page_config(page_title='🐶Home')
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."

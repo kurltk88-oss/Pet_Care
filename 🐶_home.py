4b1f6986615d5315a8613cde0cf1fc23a817546f
kurltk88-oss <kurltk88@gmail.com>
2026-01-25 22:47:10 +0000

    iconnn

--- 🐶_home.py
+++ 🐶_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title='Home',page_icon="🐶")
 
-st.image("icon.png")
+st.image("icon.png",width=40)
 st.title("Pet-care")
 dog_breeds = [
     "Affenpinscher","Afghan Hound","Aidi","Airedale Terrier","Akbash",
